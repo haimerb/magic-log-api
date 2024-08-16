e82235904fce8ca5379a815aba1e76cdaf7f3949
Haymer Barbeti <hbarbeti@in-ui.com>
2024-08-16 17:52:33 -0500

    change file server.js for add functions shoppingcart

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  //origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 const db = require("./app/models");
+const Role = db.role;
+
 db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
@@ -22,6 +27,7 @@ db.sequelize.sync()
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
+  initial();
 });
 
 app.get("/", (req, res) => {
@@ -31,7 +37,7 @@ app.get("/", (req, res) => {
 require("./app/routes/user.routes")(app);
 require("./app/routes/product.routes")(app);
 require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
+//require('./app/routes/user.routes')(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
