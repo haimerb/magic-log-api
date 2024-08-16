ff120b8e01e298078ea8077de55a3a2fb9452d7b
Haymer Barbeti <hbarbeti@in-ui.com>
2024-08-16 17:54:22 -0500

    add file app/models/shoppingcart.model.js for add functions shoppingcart

---
+++ app/models/shoppingcart.model.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, Sequelize) => {
+  const ShoppingCart = sequelize.define("shopping_cart", {
+    name: {
+      type:Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING      
+    },
+  });
+
+  return ShoppingCart;
+};
